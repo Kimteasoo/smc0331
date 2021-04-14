3ffd164ce4a0e5cb9e047f7ff354fc31fae25de7
namejin <namejin@gamil.co.kr>
2021-04-14 15:47:24 +0900

    game

---
+++ 0414/src/Game.java
@@ -0,0 +1,45 @@
+
+public class Game {
+
+	public static void game(int user, int com) {
+
+		String users = null;
+		switch (user) {
+		
+		case 1:
+			users = "가위!";
+			break;
+		case 2:
+			users = "바위!";
+			break;
+		case 3:
+			users = "보자기!";
+			break;
+		}
+	
+		String coms = null;
+		switch (com) {
+
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+} 
